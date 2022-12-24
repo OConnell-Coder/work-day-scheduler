7f8c442a1f82588bb21fb30ff60c1f966c7190bd
Codename O'Connell <oconnellcoder@gmail.com>
2022-12-23 20:39:20 -0500

    added eventlistener to btn-nine save-btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,33 @@
+var entireSchedule = [];
+var btnNineEl = $("#btn-nine");
+// console.log(typeof(btnNineEl));
+var txtNine = $("#txt-nine");
+// var txtNine = document.getElementById("txt-nine");
+
+btnNineEl.on("click", function() {
+
+  var hourNine = {
+    hour: this.id,
+    text: txtNine.val()
+  }
+
+  entireSchedule.push(hourNine);
+  localStorage.setItem("localSchedule", JSON.stringify(entireSchedule))
+  
+})
+
+
+
+// $(function () {
+
+// });
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +46,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
